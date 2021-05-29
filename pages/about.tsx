50141b6617d8469c3801c4381531a97677be1ad0
vihar <vihar.kurama@gmail.com>
2021-05-30 01:52:42 +0530

    about page content and social links

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,6 @@
+// link
+import Link from 'next/link';
+
 // layout
 import { SiteLayout } from 'layouts';
 // react bootstrap
@@ -6,31 +9,82 @@ import { Image } from 'react-bootstrap';
 function AboutPage() {
   return (
     <SiteLayout>
-      <Image
-        className="rounded-3 img-fluid my-5 mx-auto d-block"
-        src="/vihar_color.jpg"
-        width="300"
-      />
+      <div className="my-5 pt-5">
+        <div>
+          <Image
+            className="rounded-3 img-fluid my-2 mx-auto d-block"
+            src="/vihar_color.jpg"
+            width="300"
+          />
 
-      <p className="text-muted p-small">
-        Hi, I'm Vihar Kurama. I'm a developer, writer, and creator.
-        <br />
-        <br />
-        Right now, I'm building scalable products at Caravel.AI and Soulpage IT. My primary work
-        involves managing dev teams, architecturing applications and doing heavy frontend
-        engineering.
-        <br />
-        <br />
-        I'm also passionate about Machine Learning as well. I'm consulting at Eprosoft America Inc,
+          <p className="text-muted">
+            <div className="d-flex justify-content-center">
+              <div className="p-2">
+                <Link href="https://twitter.com/vihar13k">
+                  <a target="_blank" className="twitter">
+                    <i className="bi bi-twitter"></i>
+                  </a>
+                </Link>
+              </div>
+              <div className="p-2 ps-0">
+                <Link href="https://www.linkedin.com/in/vihar-kurama/">
+                  <a target="_blank" className="text-decoration-none linkedin">
+                    <i className="bi bi-linkedin"></i>
+                  </a>
+                </Link>
+              </div>
+              <div className="p-2">
+                <Link href="mailto:vihar.kurama@gmail.com">
+                  <a target="_blank" className="gmail">
+                    <i className="bi bi-envelope-fill"></i>
+                  </a>
+                </Link>
+              </div>
+              <div className="p-2">
+                <Link href="https://www.instagram.com/vihar_kurama/">
+                  <a target="_blank" className="instagram">
+                    <i className="bi bi-instagram"></i>
+                  </a>
+                </Link>
+              </div>
+              <div className="p-2">
+                <Link href="https://github.com/vihar">
+                  <a target="_blank" className="github">
+                    <i className="bi bi-github"></i>
+                  </a>
+                </Link>
+              </div>
+              <div className="p-2">
+                <Link href="https://www.reddit.com/user/vihar_kurama3">
+                  <a target="_blank" className="reddit">
+                    <i className="bi bi-reddit"></i>
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </p>
+        </div>
+
+        <p className="p-small text-muted">
+          👋 Hey, I'm Vihar Kurama. I'm a developer, writer, and creator. I'm currently working as
+          an Engineer and Developer Advocate at Appsmith.
+          <br />
+          <br />
+          🕒 In the past, I've worked with several startups as a consultant helping them build
+          scalable products. I've managed dev teams, architectured products and have done some
+          severe frontend engineering.
+          <br />
+          <br />
+          ✍️ Apart from these, I've written several articles relating to Programming Languages, Web
+          Development, and Machine Learning at Auth0, Paperspace, BuiltIn, Medium and Nanonets.
+          <br />
+          <br />☕ You can find me on Twitter, where I talk about design and development, or on
+          GitHub.
+          {/* I'm also passionate about Machine Learning as well. I'm consulting at Eprosoft America Inc,
         where I lead the ML division, working on projects related to Information Extraction, NER and
-        Speech.
-        <br />
-        <br />
-        Apart from these, I've written several articles relating to programming languages, web
-        development and machine learning at Auth0, Paperspace, BuiltIn, Medium and Nanonets. <br />
-        You can find me on Twitter where I talk about design and development, or on GitHub, where
-        I'm building in the open.
-      </p>
+        Speech. */}
+        </p>
+      </div>
     </SiteLayout>
   );
 }
--- pages/writing.tsx
+++ pages/writing.tsx
@@ -23,42 +23,45 @@ function WritingPage() {
             Web, Machine Learning, Low Code and many more.
           </p>
         </div>
-        {all_articles.map((item, index) => {
-          return (
-            <>
-              <Link href={item.url}>
-                <a className="text-decoration-none" target="_blank">
-                  <div
-                    className="card border-0 shadow-sm p-3 mb-3 border-start"
-                    style={{
-                      borderLeft: `3px solid ${
-                        article_style(item.published_at).border_color
-                      } !important`
-                    }}
-                    key={index}
-                  >
-                    <p className="card-title text-capitalize m-0">{item.title}</p>
 
-                    <div className="d-flex align-items-center">
-                      <div>
-                        <p className="m-0 p-smaller text-muted">
-                          Published on: {item.published_on}
-                        </p>
-                      </div>
-                      <div className="ms-auto">
-                        <img
-                          className="img-fluid"
-                          src={article_style(item.published_at).image_url}
-                          width="30"
-                        />
+        <div>
+          {all_articles.map((item, index) => {
+            return (
+              <>
+                <Link href={item.url}>
+                  <a className="text-decoration-none" target="_blank">
+                    <div
+                      className="card border-0 shadow-sm p-3 mb-3 border-start"
+                      style={{
+                        borderLeft: `3px solid ${
+                          article_style(item.published_at).border_color
+                        } !important`
+                      }}
+                      key={index}
+                    >
+                      <p className="card-title text-capitalize m-0">{item.title}</p>
+
+                      <div className="d-flex align-items-center">
+                        <div>
+                          <p className="m-0 p-smaller text-muted">
+                            Published on: {item.published_on}
+                          </p>
+                        </div>
+                        <div className="ms-auto">
+                          <img
+                            className="img-fluid"
+                            src={article_style(item.published_at).image_url}
+                            width="30"
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </a>
-              </Link>
-            </>
-          );
-        })}
+                  </a>
+                </Link>
+              </>
+            );
+          })}
+        </div>
       </SiteLayout>
     </>
   );
